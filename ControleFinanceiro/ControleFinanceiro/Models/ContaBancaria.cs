0ffc4c95e16b9e0a72c67bbcbac7e0aab606d44a
Nizzola <marcio@nizzola.com.br>
2019-02-26 20:54:12 -0300

    Commit inicial

---
+++ ControleFinanceiro/ControleFinanceiro/Models/ContaBancaria.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ControleFinanceiro.Models
+{
+    public class ContaBancaria
+    {
+        public int ContaBancariaId { get; set; }
+
+        public int Agencia { get; set; }
+        public int DigitoAgencia { get; set; }
+        public int Numero { get; set; }
+        public int DigitoNumero { get; set; }
+
+        public decimal Saldo { get; set; }
+
+        public bool Ativa { get; set; }
+        public DateTime DataAbertura { get; set; }
+        public DateTime? DataEncerramento { get; set; }
+
+        public Cliente Cliente { get; set; }
+    }
+}
---
+++ ControleFinanceiro/ControleFinanceiro/Views/FormCliente.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ControleFinanceiro.Views
+{
+    public partial class FormCliente : Form
+    {
+        public FormCliente()
+        {
+            InitializeComponent();
+        }
+
+        private void btnSalvar_Click(object sender, EventArgs e)
+        {
+            if (!Validate())
+                return;
+        }
+
+        private bool Validate()
+        {
+            if( ! Helpers.ValidaCpf( mskCpf.Text ))
+            {
+                MessageBox.Show("O CPF não é válido !");
+                return false;
+            }
+
+            return true;
+        }
+
+    }
+}
